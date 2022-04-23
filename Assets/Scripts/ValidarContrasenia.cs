525aca48e34a9f8d0ec9154964835bf794f26801
FaustoCitera <101577508+FaustoCitera@users.noreply.github.com>
2022-04-23 14:17:35 -0300

    video 5 terminado

--- Assets/Scripts/ValidarContrasenia.cs
+++ Assets/Scripts/ValidarContrasenia.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ValidarContrasenia : MonoBehaviour
 {
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        contraseniaCorrecta = "12345";
     }
 
     // Update is called once per frame
@@ -19,6 +25,14 @@ public class ValidarContrasenia : MonoBehaviour
 
     public void validarContrasenia()
 	{
-
+        contraseniaUsuario = ingresoUsuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+		{
+            Debug.Log("Bienvenido");
+		}
+        else
+		{
+            Debug.Log("Contarseña Incorrecta");
+        }
 	}
 }
